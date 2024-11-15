fe2662c43806cbba90672e09d0aaed113637654b
DaAatrox <156982425+DaAatrox@users.noreply.github.com>
2024-11-15 14:23:27 -0600

    Update main.c

--- 13-strike_zone_2/main.c
+++ 13-strike_zone_2/main.c
@@ -5,12 +5,15 @@
 
 int main(int argc, char *argv[]) {
     // TODO: Verify command line arguments
-
+    if (argc != 5){
+        printf("usage: &S <limit> <fps> <bar_size> <strike_zone>\n",argv[0];
+        return 1;
+    }
     // TODO: Parse command line arguments
-    int limit = 0;
-    int fps = 0;
-    int bar_size = 0;
-    int strike_zone = 0;
+    float limit = atof(argv[1]);
+    int fps = atoi(argv[2]);
+    int bar_size = atoi(argv[3]);
+    int strike_zone = atoi(argv[4]);
 
     clock_t start = clock();
     clock_t elapsed_time = clock() - start;
@@ -18,7 +21,8 @@ int main(int argc, char *argv[]) {
     while (elapsed_time < limit * CLOCKS_PER_SEC) {
         elapsed_time = clock() - start;
         if (elapsed_time % (CLOCKS_PER_SEC / fps) == 0) {
-            printf("%ld\n", elapsed_time * 1000 / CLOCKS_PER_SEC);
+            printf("\033[2l\033[1;1h");
+            print_bar(bar_size, (float) elapsed_time / (limit * CLOCKS_PER_SEC), strike_zone);
         }
     }
 }
